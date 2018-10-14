5c4d001aadb975b55af34386994eefa1f4a89a3e
dotm <desde.el.mar96@gmail.com>
2018-10-14 19:04:04 +0700

    Initial Commit

---
+++ Ourganic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ourganic
+//
+//  Created by Yoshua Elmaryono on 14/10/18.
+//  Copyright © 2018 Yoshua Elmaryono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
